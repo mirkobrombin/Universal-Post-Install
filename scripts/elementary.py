ee622d33a1c110253f57d3a03fba2ffcd07ba457
Mirko Brombin <brombinmirko@gmail.com>
2017-09-02 17:13:41 +0200

    Modified Post-Installation script syntax. Inside, the PostInstall class has been added, which contains all the functions and parameters for the steps. The PostInstall class is passed as a pi variable to Helper via the steps function. This allows you to call functions from different files.

--- scripts/elementary.py
+++ scripts/elementary.py
@@ -18,16 +18,49 @@
 '''
 import helper
 
-helper.p_title("ElementaryOS post installation script by https://linuxhub.it")
+# Define the package manager
+E = "apt"
 
+helper.title("ElementaryOS post installation script by https://linuxhub.it")
+
+# Check for release
 if helper.get_distro().codename == "loki":
-    helper.p_info("Loki detected")
+    helper.info("Loki detected")
+    class PostInstall:
+        global E
+        # Define menu voices
+        voices = [
+            ("Install Elementary Tweaks", "install_elementary_tweaks"),
+            ("Install Gimp", "install_gimp"), 
+            ("Install Thunderbird", "install_thunderbird"),
+        ]
+        
+        # Define functions for each menu voice
+        def install_elementary_tweaks(self):
+            helper.info("Installing dependence: software-properties-common")
+            helper.pkg_install("software-properties-common", E)
+            helper.info("Adding repository: philip.scott/elementary-tweaks")
+            helper.pkg_add_repo("philip.scott/elementary-tweaks", E)
+            helper.info("Updating..")
+            helper.pkg_update(E)
+            helper.info("Installing: elementary-tweaks")
+            helper.pkg_install("elementary-tweaks", E)
+
+        def install_gimp(self):
+            helper.info("Updating..")
+            helper.pkg_update(E)
+            helper.info("Installing: gimp")
+            helper.pkg_install("gimp", E)
+
+        def install_thunderbird(self):
+            helper.info("Updating..")
+            helper.pkg_update(E)
+            helper.info("Installing: thunderbird")
+            helper.pkg_install("thunderbird", E)
 else:
-    helper.p_warning("This script is not compatible with " + helper.get_distro().codename)
+    helper.warning("This script is not compatible with " + helper.get_distro().codename)
     helper.exit()
 
-voices = [
-    ('apt update', 'sudo apt update'), 
-]
-
-helper.steps(voices)
+# Load script
+pi = PostInstall()
+helper.steps(pi.voices, pi)
