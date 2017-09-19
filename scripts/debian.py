d8a05fcfbdaa88d335d29deebb2c6ad6a5b6f96e
Mirko Brombin <brombinmirko@gmail.com>
2017-09-19 22:32:41 +0200

    Added initial support for Debian 9.1.0

---
+++ scripts/debian.py
@@ -0,0 +1,152 @@
+'''
+   Copyright 2017 Mirko Brombin (brombinmirko@gmail.com)
+
+   This file is part of Universal Post Install.
+
+   Universal Post Install is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+          the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Universal Post Install is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Universal Post Install.  If not, see <http://www.gnu.org/licenses/>.
+'''
+import helper
+
+# Define the package manager
+E = "apt"
+
+helper.title("Debian")
+helper.author("Mirko Brombin")
+helper.website("https://linuxhub.it")
+
+# Check for release
+distro = helper.get_distro()
+helper.warning("The script for Debian has not been tested yet.")
+helper.warning("Production usage is not recommended at this time.")
+if distro.release == "9.1.0":
+    class PostInstall:
+        global E
+        # Define menu voices
+        voices_en_US = [
+            ("Install updates ", "install_updates"), 
+            ("Install add-apt-repository", "enable_ppa"),
+            ("Install GDebi (deb installer) ", "install_gdebi"), 
+            ("Install Qapt (deb installer) ", "install_qapt"),
+            ("Install GNOME (DE)", "install_gnome"), 
+            ("Install GNOME (DE)", "install_gnome"), 
+            ("Install KDE (DE)", "install_kde"), 
+            ("Install XFCE (DE)", "install_xfce"), 
+            ("Install MATE (DE)", "install_mate"), 
+            ("Install LXDE (DE) ", "install_lxde"), 
+            ("Install Cinnamon (DE) ", "install_cinnamon"), 
+            ("Install Budgie (DE) ", "install_budgie"), 
+            ("Install Enlightenment (DE) ", "install_enlightenment"), 
+            ("Install Openbox (WM) ", "install_openbox"), 
+            ("Install Fluxbox (WM) ", "install_fluxbox"), 
+            ("Install multimedia codecs ", "install_multimediacodecs"),
+            ("Install Microsoft core Fonts ", "install_mscorefonts"),
+        ]
+        voices_it_IT = [
+            ("Installa aggiornamenti", "install_updates"), 
+            ("Installa add-apt-repository", "enable_ppa"),
+            ("Installa GDebi (deb installer)", "install_gdebi"), 
+            ("Installa Qapt (deb installer) ", "install_qapt"), 
+            ("Installa GNOME (DE)", "install_gnome"), 
+            ("Installa KDE (DE)", "install_kde"), 
+            ("Installa XFCE (DE)", "install_xfce"), 
+            ("Installa MATE (DE)", "install_mate"), 
+            ("Installa LXDE (DE) ", "install_lxde"), 
+            ("Installa Cinnamon (DE) ", "install_cinnamon"), 
+            ("Installa Budgie (DE) ", "install_budgie"), 
+            ("Installa Enlightenment (DE) ", "install_enlightenment"), 
+            ("Installa Openbox (WM) ", "install_openbox"), 
+            ("Installa Fluxbox (WM) ", "install_fluxbox"), 
+            ("Installa codec multimediali", "install_multimediacodecs"),
+            ("Installa Microsoft core Fonts ", "install_mscorefonts"),
+        ]
+        
+        # Define functions for each menu voice
+        def install_updates(self):
+            helper.pkg_update(E)
+            helper.pkg_sys_upgrade(E)
+
+        def enable_ppa(self):
+            helper.pkg_install("software-properties-common", E)
+            helper.pkg_update(E)
+
+        def install_gdebi(self):
+            helper.pkg_install("gdebi", E)
+            helper.pkg_update(E)
+
+        def install_qapt(self):
+            helper.pkg_install("qapt-deb-installer", E)
+            helper.pkg_update(E)
+
+        def install_gnome(self):
+            helper.pkg_install("gnome", E)
+            helper.pkg_update(E)
+
+        def install_kde(self):
+            helper.pkg_install("kde-full", E)
+            helper.pkg_update(E)
+
+        def install_xfce(self):
+            helper.pkg_install("xfce4 xfce4-goodies", E)
+            helper.pkg_update(E)
+
+        def install_mate(self):
+            helper.pkg_install("mate-desktop-environment", E)
+            helper.pkg_update(E)
+
+        def install_lxde(self):
+            helper.pkg_install("lxde", E)
+            helper.pkg_update(E)
+
+        def install_cinnamon(self):
+            helper.pkg_install("cinnamon", E)
+            helper.pkg_update(E)
+
+        def install_lxqt(self):
+            helper.pkg_install("lxqt", E)
+            helper.pkg_update(E)
+
+        def install_budgie(self):
+            helper.pkg_install("budgie-desktop", E)
+            helper.pkg_update(E)
+
+        def install_openbox(self):
+            helper.pkg_install("openbox menu", E)
+            helper.pkg_update(E)
+
+        def install_fluxbox(self):
+            helper.pkg_install("fluxbox", E)
+            helper.pkg_update(E)
+
+        def install_multimediacodecs(self):
+            self.enable_ppa()
+            helper.pkg_add_repo("'deb http://www.deb-multimedia.org stretch main non-free'", E)
+            helper.pkg_install("deb-multimedia-keyring libdvdcss2", E)
+            helper.pkg_update(E)
+
+        def install_mscorefonts(self):
+            helper.do("mkdir temp", True)
+            helper.do("cd temp", True)
+            helper.do("wget http://ftp.de.debian.org/debian/pool/contrib/m/msttcorefonts/ttfmscorefonts-installer_3.6_all.deb", True)
+            helper.do("dpkg --install ttf-mscorefonts-installer_3.6_all.deb", True)
+            helper.pkg_update(E)
+else:
+    helper.not_compatible()
+
+# Load script
+pi = PostInstall()
+try:
+    voices = eval('pi.voices_' + distro.lang)
+except AttributeError:
+    voices = pi.voices_en_US
+helper.steps(voices, pi)
